64d7085e39083199b7e1c12610dbb1cdbda21815
maidaxabdi <maidaxabdi@gmail.com>
2021-10-12 15:58:34 -0500

    Finished Further Study: Upgrade Templates

---
+++ madlibs.py
@@ -0,0 +1,77 @@
+"""A madlib game that compliments its users."""
+
+from random import sample
+
+from flask import Flask, render_template, request
+
+# "__name__" is a special Python variable for the name of the current module.
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    "awesome",
+    "terrific",
+    "fantastic",
+    "neato",
+    "fantabulous",
+    "wowza",
+    "oh-so-not-meh",
+    "brilliant",
+    "ducky",
+    "coolio",
+    "incredible",
+    "wonderful",
+    "smashing",
+    "lovely",
+]
+
+
+@app.route("/")
+def start_here():
+    """Display homepage."""
+
+    return "Hi! This is the home page."
+
+
+@app.route("/hello")
+def say_hello():
+    """Say hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route("/greet")
+def greet_person():
+    """Greet user with compliment."""
+
+    player = request.args.get("person")
+
+    compliments = sample(AWESOMENESS,3)
+
+    return render_template("compliment.html", person=player, compliment=compliments)
+
+@app.route("/game")
+def show_madlib_form():
+    answer = request.args.get("response")
+    print(answer)
+    if answer == "Yes":
+        test = "game.html"
+    else:
+        test = "goodbye.html"
+    return render_template(f"{test}")
+
+@app.route("/madlib")
+def show_madlib():
+    name = request.args.get("person")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+
+    return render_template("madlib.html", person = name, color = color,
+                                    noun = noun, adjective = adjective)
+
+if __name__ == "__main__":
+    # Setting debug=True gives us error messages in the browser and also
+    # "reloads" our web app if we change the code.
+
+    app.run(debug=True, host="0.0.0.0")
